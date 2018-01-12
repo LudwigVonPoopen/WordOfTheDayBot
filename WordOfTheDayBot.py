77d2cff57487988e64a6888820627cbddfc5a71d
LudwigVonPoopen <yohaw1@gmail.com>
2018-01-12 18:36:30 -0500

    Fixes to getting the words definition

--- WordOfTheDayBot.py
+++ WordOfTheDayBot.py
@@ -18,20 +18,12 @@ definition = soup.ol.li
 
 definition_str = str(definition)
 
-x = definition_str.index("/em")
-y = definition_str.index(".</span>")
+x = definition_str.index("<span>")
+y = definition_str.index("<", x+1)
 
-definition_str = definition_str[x:y]
+definition_str = definition_str[x+6:y]
 
-z = definition_str.index(" ")
-test = definition_str[z+1:len(definition_str)]
-
-word = soup.title.string
-
-word = word.replace("Get the Word of the Day - ", "")
-word = word.replace(" | Dictionary.com", "")
-
-post_text = word + ": " + test
+post_text = word + ": " + definition_str
 
 reddit = praw.Reddit(username = config.username,
                      password = config.password,
